f0c9b1388d871cc60e595f681538439e95ff253d
Niveytha Waran <niveytha@gmail.com>
2022-05-17 16:45:39 +0800

    Completed

--- first-electron-app/main.js
+++ first-electron-app/main.js
@@ -1,31 +1,29 @@
-const { app, BrowserWindow } = require('electron') // controls application's event lifecycle
 const path = require('path')
 
-// creates and manages application windows
+const { app, BrowserWindow } = require('electron')
+
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
       height: 600,
       webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-        // _dirname: points to the path of the currently executing script
-        // path.join:  joins multiple path segments together, creating a combined path string that works across all platforms
+          // __dirname -> points to path of currently executing script
+          // path.join() -> API joins multiple path segments together
+          preload: path.join(__dirname, 'preload.js')
       }
     })
   
     win.loadFile('index.html')
   }
 
-// open window
-app.whenReady().then(() => {
+  app.whenReady().then(() => {
     createWindow()
-
+  
     app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
   })
 
-// exiting window quits app entirely
-app.on('window-all-closed', () => {
+  app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
-})
+  })
