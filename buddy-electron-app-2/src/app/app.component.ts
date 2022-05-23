8bbf1b6411489a625fb5dd8e03ada00f5b5938c5
Niveytha Waran <niveytha@gmail.com>
2022-05-23 17:21:39 +0800

    buddy-electron-app-2

---
+++ buddy-electron-app-2/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'buddy-electron-app-2';
+  constructor(private _electronService: ElectronService) { }
+  
+}
