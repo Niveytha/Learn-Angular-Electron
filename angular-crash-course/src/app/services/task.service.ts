8b4d57b609447dce02ff7a9df21b84fddb553baf
Niveytha Waran <niveytha@gmail.com>
2022-05-24 14:07:46 +0800

    json server fixed

--- angular-crash-course/src/app/services/task.service.ts
+++ angular-crash-course/src/app/services/task.service.ts
@@ -12,7 +12,7 @@ export class TaskService {
   constructor() { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    // const tasks = of(TASKS);
+    return of(TASKS);
   }
 }
