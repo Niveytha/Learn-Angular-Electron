602936507a8b443b92ef76b306aaeaf3d41f5075
Niveytha Waran <niveytha@gmail.com>
2022-05-25 10:28:35 +0800

    Deleted app duplicate

--- angular-electron-crash-course/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({  // component declaration
-  selector: 'app-root',                 // HTML tag
-  templateUrl: './app.component.html',  // HTML file
-  styleUrls: ['./app.component.css']    // CSS file
-})
-export class AppComponent {   // properties of component
-}
