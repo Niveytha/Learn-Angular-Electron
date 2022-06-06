169c44b4166ed9e6cda793b9dd00bad7270cf14c
Niveytha Waran <niveytha@gmail.com>
2022-06-06 10:49:05 +0800

    Turned Angular app into Electron app

--- keysight-data-simulator/src/app/app.component.ts
+++ keysight-data-simulator/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'keysight-data-simulator';
+  title : string = 'Keysight Data Simulator';
 }
