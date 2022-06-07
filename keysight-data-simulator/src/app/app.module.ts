0115448797f130e0be4028f6fb28330e3b9b815f
Niveytha Waran <niveytha@gmail.com>
2022-06-07 16:53:41 +0800

    Skeleton of first screen done

--- keysight-data-simulator/src/app/app.module.ts
+++ keysight-data-simulator/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    MainContentComponent
+    MainContentComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
