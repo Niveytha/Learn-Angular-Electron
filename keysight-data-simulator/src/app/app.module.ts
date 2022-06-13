7a0336f8032668545c0540934b13fe91a34d20e0
Niveytha Waran <niveytha@gmail.com>
2022-06-13 23:06:58 +0800

    Routing done

--- keysight-data-simulator/src/app/app.module.ts
+++ keysight-data-simulator/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { ButtonComponent } from './components/button/button.component';
 import { BatchFormComponent } from './components/forms/batch-form/batch-form.component';
 import { BtestFormComponent } from './components/forms/btest-form/btest-form.component';
 import { AlertSimulationComponent } from './components/alert-simulation/alert-simulation.component';
+import { DropdownMenuComponent } from './components/dropdown-menu/dropdown-menu.component';
+import { FinalPageComponent } from './components/final-page/final-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainContentComponent },
+  { path: 'alert-simulation', component: AlertSimulationComponent },
+  { path: 'end', component: FinalPageComponent }
+]
 
 @NgModule({
   declarations: [
@@ -18,11 +27,14 @@ import { AlertSimulationComponent } from './components/alert-simulation/alert-si
     ButtonComponent,
     BatchFormComponent,
     BtestFormComponent,
-    AlertSimulationComponent
+    AlertSimulationComponent,
+    DropdownMenuComponent,
+    FinalPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
