89e1e05415ce751d1ad55794535e5316b9a364cf
Niveytha Waran <niveytha@gmail.com>
2022-06-13 16:31:31 +0800

    Cleanup

--- keysight-data-simulator/src/app/components/forms/batch-form/batch-form.component.ts
+++ keysight-data-simulator/src/app/components/forms/batch-form/batch-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+// const express = require('express');
 
 @Component({
-  selector: 'app-batch-form',
-  templateUrl: './batch-form.component.html',
-  styleUrls: ['./batch-form.component.css']
+    selector: 'app-batch-form',
+    templateUrl: './batch-form.component.html',
+    styleUrls: ['./batch-form.component.css']
 })
+
 export class BatchFormComponent implements OnInit {
+    title: string = '1479299-00-D';
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+    }
 }
